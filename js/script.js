2cbcba1c28663ff906b26b798ccee833d8d7b266
Constantine <asirvex@gmail.com>
2018-10-10 12:40:46 +0300

    [161039550] Add select element and text area styles

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 function showAll(){
+    let el = document.getElementById("phones");
+    el.style.display = "none"
+}
+
+function show(el){
+    el.style.display = "inline"
+}
+
+function showPhones(){
+    let ph = document.getElementById("phones");
+    show(ph)
 }
