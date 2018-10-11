3d08168a6ccc1a9d18ff0c11c0b6abe8bc6c6102
Constantine <asirvex@gmail.com>
2018-10-12 01:29:13 +0300

    [#161039610] Add products to the all tab

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 function showAll(){
     let el = document.getElementById("phones");
-    el.style.display = "none"
+    el.style.display = "none";
+    let all = document.getElementById("all");
+    all.style.display ="block";
 }
 
 function show(el){
-    el.style.display = "inline"
+    el.style.display = "inline";
 }
 
 function showPhones(){
+    let all = document.getElementById("all");
+    all.style.display ="none";
     let ph = document.getElementById("phones");
     show(ph)
 }
