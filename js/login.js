0f3dbcf18c60e7eb8ec8abb6beb1cde6b003f485
asirvex <asirvex@gmail.com>
2018-11-10 10:11:52 +0300

    [#161039592] power the login page using javascript fetch api

---
+++ js/login.js
@@ -0,0 +1,40 @@
+document.getElementById('login-fm').addEventListener
+("submit", login);
+
+function login(e){
+    e.preventDefault();
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("pwd").value;
+    let token;
+    fetch('http://127.0.0.1:5000/api/v2/auth/login', {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+    })
+    .then(handleResponse)
+    .then(data => {
+        token = data.token;
+        window.location.href = "products.html";
+    })
+    .catch(error => document.getElementById("login-title").innerHTML = `
+        <h1>login</h1>
+        <h3>${error.message}</h3>`);
+
+    function handleResponse(response) {
+        return response.json()
+          .then(json => {
+            if (response.ok) {
+              return json
+            } else {
+              return Promise.reject(json)
+            }
+        })
+
+    }
+
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function showAll(){
     let el = document.getElementById("phones");
     el.style.display = "none"
@@ -11,3 +12,52 @@ function showPhones(){
     let ph = document.getElementById("phones");
     show(ph)
 }
+=======
+function showAll(){
+    let el = document.getElementById("phones");
+    el.style.display = "none";
+    let all = document.getElementById("all");
+    all.style.display ="block";
+}
+
+function show(el){
+    el.style.display = "inline";
+}
+
+function showPhones(){
+    let all = document.getElementById("all");
+    all.style.display ="none";
+    let ph = document.getElementById("phones");
+    show(ph)
+}
+
+function goBack() {
+    window.history.back();
+}
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("addbtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+>>>>>>> Stashed changes
